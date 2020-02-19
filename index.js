752d0ef27e2906b6261d84f5a458ba3130dc7533
Jozi <1612872@rgu.ac.uk>
2020-02-19 11:38:31 +0000

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var http = require("http");
+fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 
 http.createServer(function(request,response){
 
 
-  response.writeHead(200,{'Content-Type':'text/plain'})
+  response.writeHead(200,{'Content-Type':'text/html','Access-Control-Allow-Origin','*'})
 
-  response.end('Hello World\n');
+  var readStream = fs.createReadStream('/index.html');
+
+  readStream.pipe(response);
+
+  //response.end('Hello World\n');
 
   console.log(PORT);
 
