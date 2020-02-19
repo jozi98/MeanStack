14e618e489fc77617d7f444dc43a1a2c08e7f7cb
Jozi <1612872@rgu.ac.uk>
2020-02-19 13:40:03 +0000

    .use

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ adminRouter.get('/posts', function(req, res) {
  res.send('I show all the posts!'); });
 // apply the routes to our application
 app.use('/admin', adminRouter);
+
+///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+ adminRouter.use(function(req, res, next) {
+ // log each request to the console
+ console.log(req.method, req.url);
+ // continue doing what we were doing and go to the route
+ next(); });
+//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////
 });
 // start the server
