02c0a0941223555037420a2d84e010efddaea49a
Jozi <1612872@rgu.ac.uk>
2020-02-19 11:47:46 +0000

    colon

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 http.createServer(function(request,response){
 
 
-  response.writeHead(200,{'Content-Type':'text/html','Access-Control-Allow-Origin','*'})
+  response.writeHead(200,{'Content-Type':'text/html','Access-Control-Allow-Origin':'*'})
 
   var readStream = fs.createReadStream('MeanStack/index.html');
 
